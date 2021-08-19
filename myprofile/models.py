626bd07f29b03bde087e95942a17f567e2bfe99d
MeMeee1 <ykatchy@gmail.com>
2021-08-19 03:14:07 +0100

    Update UI

--- myprofile/models.py
+++ myprofile/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class New(models.Model):
 	name = models.TextField()
-	email= models.TextField()
+	email= models.EmailField()
 	comment = models.TextField()
 
 def __str__(self):
