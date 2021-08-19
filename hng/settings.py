3725b4ea020578559bd3a0c56d61402bb1e25923
MeMeee1 <ykatchy@gmail.com>
2021-08-19 02:53:35 +0100

    deploy to Heroku

--- hng/settings.py
+++ hng/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(ly3h&jo@*@l)n2s1i0(vcy9s_im81@)$u7indl2m00r^7219z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
